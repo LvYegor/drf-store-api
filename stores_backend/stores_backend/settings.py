e1b8817b69d35738288d36810615ed07a97e5e80
Yahor Andryieuski <yahor.andryieuski@gmail.com>
2024-08-08 01:19:40 +0300

    Dockerize app. Add Dockerfile and docker-compose

--- stores_backend/stores_backend/settings.py
+++ stores_backend/stores_backend/settings.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -83,17 +84,27 @@ WSGI_APPLICATION = 'stores_backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'stores_db',
+#         'USER': 'postgres',
+#         'PASSWORD': '020508',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432'
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'stores_db',
-        'USER': 'postgres',
-        'PASSWORD': '020508',
-        'HOST': '127.0.0.1',
-        'PORT': '5432'
+        'ENGINE': os.environ.get('DB_DRIVER', 'django.db.backends.postgresql'),
+        'NAME': os.environ.get('PG_DB', 'stores_db'),
+        'USER': os.environ.get('PG_USER', 'postgres'),
+        'PASSWORD': os.environ.get('PG_PASSWORD', '020508'),
+        'HOST': os.environ.get('PG_HOST', 'localhost'),     # uses the container if set, otherwise it runs locally
+        'PORT': os.environ.get('PG_PORT', '5432'),
     }
 }
-
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
