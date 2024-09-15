c8259fe2bbb2d10dfc47a88806e96b1fd6991a79
Yahor Andryieuski <yahor.andryieuski@gmail.com>
2024-09-15 12:31:03 +0300

    Add SerializerMethodField for StoresSerializer and CommentSerializer

--- stores_backend/backend_api/serializers.py
+++ stores_backend/backend_api/serializers.py
@@ -1,11 +1,17 @@
 from rest_framework import serializers
 from .models import *
+from django.utils.timezone import now
 
 
 class StoresSerializer(serializers.ModelSerializer):
+    store_lifetime = serializers.SerializerMethodField()
+
     class Meta:
         model = Store
         fields = '__all__'
+
+    def get_store_lifetime(self, obj):
+        return (now().date() - obj.established).days
 
 
 class ProductSerializer(serializers.ModelSerializer):
@@ -15,6 +21,12 @@ class ProductSerializer(serializers.ModelSerializer):
 
 
 class CommentSerializer(serializers.ModelSerializer):
+    comment_lifetime = serializers.SerializerMethodField()
+
     class Meta:
         model = ProductComment
         fields = '__all__'
+
+    def get_comment_lifetime(self, obj):
+
+        return (now() - obj.posted).days
